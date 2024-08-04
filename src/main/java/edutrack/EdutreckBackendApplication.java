5087c77297ad53ffb2fc40868a27d224aa8bedec
Dmitrii <zhirov.i.am@gmail.com>
2024-08-04 17:44:04 +0300

    Initial commit

---
+++ src/main/java/edutrack/EdutreckBackendApplication.java
@@ -0,0 +1,13 @@
+package edutrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutreckBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdutreckBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edutrack/EdutreckBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edutrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutreckBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
