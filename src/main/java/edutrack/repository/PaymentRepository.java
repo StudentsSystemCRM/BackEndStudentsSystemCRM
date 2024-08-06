1b0ed050fa1a99440af88d3375264c10ae83a656
Larisa Becket <lbikteeva@gmail.com>
2024-08-07 01:49:43 +0300

    Added entities (Student, Group, TestEntity, AnotherEntity) and corresponding repositories

--- src/main/java/edutrack/repository/PaymentRepository.java
+++ src/main/java/edutrack/repository/PaymentRepository.java
@@ -3,6 +3,5 @@ package edutrack.repository;
 import edutrack.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
