c07faf5cfa4939dc2a1fd4b0e94434f6f8ce4b26
Larisa Becket <lbikteeva@gmail.com>
2024-08-07 01:45:45 +0300

    Added entities (Student, Group, TestEntity, AnotherEntity) with corresponding repositories

--- src/main/java/edutrack/entity/Group.java
+++ src/main/java/edutrack/entity/Group.java
@@ -27,5 +27,4 @@ public class Group {
 
     @OneToMany(mappedBy = "group")
     private List<Student> students;
-
 }
--- src/main/java/edutrack/repository/StudentRepository.java
+++ src/main/java/edutrack/repository/StudentRepository.java
@@ -2,5 +2,6 @@ package edutrack.repository;
 import edutrack.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
