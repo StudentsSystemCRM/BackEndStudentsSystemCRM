cf55481f1f279f7ce4ed593bc929ab26a04df9ab
Ekaterina Goncharenko <eg93006@gmail.com>
2024-08-14 14:28:36 +0300

    fix bag dto accounting

--- src/main/java/edutrack/dto/request/accounting/PasswordUpdateRequest.java
+++ src/main/java/edutrack/dto/request/accounting/PasswordUpdateRequest.java
@@ -1,5 +1,17 @@
 package edutrack.dto.request.accounting;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class PasswordUpdateRequest {
 	
 	String password;
