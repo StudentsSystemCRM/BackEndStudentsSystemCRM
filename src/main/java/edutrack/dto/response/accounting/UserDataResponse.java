9c84bc7179fc6622933743ab057637687bb009ae
Ekaterina Goncharenko <eg93006@gmail.com>
2024-08-15 14:44:47 +0300

    add security

--- src/main/java/edutrack/dto/response/accounting/UserDataResponse.java
+++ src/main/java/edutrack/dto/response/accounting/UserDataResponse.java
@@ -8,10 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
