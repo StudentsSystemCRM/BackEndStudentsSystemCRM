e7a801cebae215c2182fcfae95421ca8050db89b
Dmitrii <zhirov.i.am@gmail.com>
2024-08-17 17:09:33 +0300

    refactoring

--- src/main/java/edutrack/dto/request/students/StudentUpdateDataRequest.java
+++ src/main/java/edutrack/dto/request/students/StudentUpdateDataRequest.java
@@ -1,37 +1,24 @@
 package edutrack.dto.request.students;
 
-import edutrack.constant.ValidAccountConstant;
-import edutrack.constant.ValidationAccountingMessage;
-
 import jakarta.validation.constraints.*;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+
+import static edutrack.constant.ValidAccountConstant.NAME_PATTERN;
+import static edutrack.constant.ValidAccountConstant.PHONE_NUMBER_PATTERN;
+import static edutrack.constant.ValidationAccountingMessage.*;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentUpdateDataRequest {
-	@NotNull(message = "ID cannot be null")
 	Long id;
-
-	@NotBlank(message = ValidationAccountingMessage.NULL_NAME)
-	@Pattern(regexp = ValidAccountConstant.NAME_PATTERN, message = ValidationAccountingMessage.INVALID_NAME)
 	String name;
-
-	@NotBlank(message = ValidationAccountingMessage.NULL_NAME)
-	@Pattern(regexp = ValidAccountConstant.NAME_PATTERN, message = ValidationAccountingMessage.INVALID_NAME)
 	String surname;
-
-	@NotBlank(message = ValidationAccountingMessage.NULL_PHONE)
-	@Pattern(regexp = ValidAccountConstant.PHONE_NUMBER_PATTERN, message = ValidationAccountingMessage.INVALID_PHONE)
 	String phone;
-
-	@NotBlank(message = ValidationAccountingMessage.BLANK_EMAIL)
-	@Email(message = ValidationAccountingMessage.INVALID_EMAIL)
 	String email;
-
 	String city;
 	String course;
 	String source;
