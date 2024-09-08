69e529d39267f7652be13234b04d337b3c830f36
AlexRy <rublue2013@gmail.com>
2024-09-08 21:44:59 +0300

    add StudentReminderService

---
+++ src/main/java/edutrack/modul/reminder/dto/response/ReminderResponse.java
@@ -0,0 +1,19 @@
+package edutrack.modul.reminder.dto.response;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReminderResponse {
+	
+	Long id;
+	List<SingleReminder> reminders;
+}
--- src/main/java/edutrack/modul/reminder/repository/StudentRemindersRepository.java
+++ src/main/java/edutrack/modul/reminder/repository/StudentRemindersRepository.java
@@ -1,8 +1,12 @@
 package edutrack.modul.reminder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edutrack.modul.reminder.entity.StudentReminders;
 
 public interface StudentRemindersRepository extends JpaRepository<StudentReminders, Long> {
+	
+	List<StudentReminders> findByStudentId (Long id);
 }
