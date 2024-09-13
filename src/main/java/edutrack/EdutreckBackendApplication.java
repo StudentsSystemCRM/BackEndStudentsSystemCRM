c987f84c3d0dd9a65d0cc22b9f761e8331210ec2
AlexRy <rublue2013@gmail.com>
2024-09-13 16:56:30 +0300

    test push

--- src/main/java/edutrack/EdutreckBackendApplication.java
+++ src/main/java/edutrack/EdutreckBackendApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EdutreckBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EdutreckBackendApplication.class, args);
-        
-        
+        SpringApplication.run(EdutreckBackendApplication.class, args);    
     }
 }
