f6ede475657e9642c7ddf38b1535349f0ddc8fba
Dmitrii <zhirov.i.am@gmail.com>
2024-09-17 23:42:33 +0300

    ver.1.4 upd application properties, JwtUtils and AuthController. create classes for refresh token (entity, repo, service, exception).

--- src/main/java/edutrack/security/jwt/JwtUtils.java
+++ src/main/java/edutrack/security/jwt/JwtUtils.java
@@ -1,6 +1,7 @@
 package edutrack.security.jwt;
 
 import edutrack.security.services.UserDetailsImpl;
+import edutrack.user.entity.UserEntity;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -29,25 +30,24 @@ public class JwtUtils {
     @Value("${jwt.cookieName}")
     private String jwtCookie;
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+    @Value("${jwt.refreshCookieName}")
+    private String jwtRefreshCookie;
 
-    public String getJwtFromCookies(HttpServletRequest request) {
-        Cookie cookie = WebUtils.getCookie(request, jwtCookie); // get token from cookie
-        if (cookie != null) {
-            return cookie.getValue();
-        } else {
-            return null;
-        }
-    }
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
     // generate token after success authentication user (for login method - look at AuthController)
     public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
         String jwt = generateTokenFromUsername(userPrincipal.getUsername());        // create cookie
         logger.info("JWT token generated for user: {}", userPrincipal.getUsername());
 
-        return ResponseCookie.from(jwtCookie, jwt)                  // create cookie with token
-                .path("/api").maxAge(24 * 60 * 60)    // access for all paths, and time live = 24 hours
-                .httpOnly(true).build();                            // HttpOnly for security
+        return generateCookie(jwtCookie, jwt, "/api");
+    }
+
+    public ResponseCookie generateJwtCookie(String userEmail) {
+        String jwt = generateTokenFromUsername(userEmail);
+        logger.info("JWT token generated for userEntity: {}", userEmail);
+
+        return generateCookie(jwtCookie, jwt, "/api");
     }
 
     // generate token bu email (username = email)
@@ -55,28 +55,47 @@ public class JwtUtils {
         Instant now = Instant.now(Clock.systemUTC());
         logger.info("Token generated at UTC time: {}", now);
 
-        return Jwts.builder().setSubject(username)
+        return Jwts.builder()
+                .setSubject(username)
                 .setIssuedAt(Date.from(now))
                 .setExpiration(Date.from(now.plusMillis(jwtExpirationMs)))
                 .signWith(key(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
+    public ResponseCookie generateRefreshJwtCookie(String refreshToken) {
+        return  generateCookie(jwtRefreshCookie, refreshToken, "/api/auth/refreshtoken");
+    }
+
+    public String getJwtFromCookies(HttpServletRequest request) {
+        return getCookieValueByName(request, jwtCookie);
+    }
+
+    public String getJwtRefreshFromCookies(HttpServletRequest request) {
+        return getCookieValueByName(request, jwtRefreshCookie);
+    }
+
     // get email method from JWT (email = username)
     public String getUserNameFromJwtToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key()).build()
-                .parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     // create cookie without JWT (for logout - look at AuthController)
     public ResponseCookie getCleanJwtCookie() {
         return ResponseCookie.from(jwtCookie, null)
-                .path("/api").build();
+                .path("/api")
+                .build();
     }
 
-    // create the key for subscribe JWT
-    private Key key() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    public ResponseCookie getCleanJwtRefreshCookie() {
+        return ResponseCookie.from(jwtRefreshCookie, null)
+                .path("/api/auth/refreshtoken")
+                .build();
     }
 
     public boolean validateJwtToken(String authToken) {
@@ -84,8 +103,8 @@ public class JwtUtils {
             Instant now = Instant.now(Clock.systemUTC());
             logger.info("Current UTC time: {}", now);
 
-            Jwts.parserBuilder().setSigningKey(key())
-                    .setAllowedClockSkewSeconds(60)
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
                     .build().parse(authToken);
 
             logger.info("JWT token is valid");
@@ -100,5 +119,26 @@ public class JwtUtils {
             logger.error("JWT claims string is empty: {}", e.getMessage());
         }
         return false;
+    }
+
+    // create the key for subscribe JWT
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    private ResponseCookie generateCookie(String name, String value, String path) {
+        return ResponseCookie.from(name, value)       // create cookie with token
+                .path(path)                           // access for all paths
+                .maxAge(24 * 60 * 60)   // time live = 24 hours
+                .httpOnly(true).build();              // HttpOnly for security
+    }
+
+    private String getCookieValueByName(HttpServletRequest request, String name) {
+        Cookie cookie = WebUtils.getCookie(request, name);  // get token from cookie
+        if (cookie != null) {
+            return cookie.getValue();
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/edutrack/user/controller/AuthController.java
+++ src/main/java/edutrack/user/controller/AuthController.java
@@ -1,7 +1,9 @@
 package edutrack.user.controller;
 
+import edutrack.security.entity.RefreshTokenEntity;
 import edutrack.security.jwt.AuthTokenFilter;
 import edutrack.security.jwt.JwtUtils;
+import edutrack.security.services.RefreshTokenService;
 import edutrack.security.services.UserDetailsImpl;
 import edutrack.user.dto.request.LoginRequest;
 import edutrack.user.dto.request.UserRegisterRequest;
@@ -9,6 +11,7 @@ import edutrack.user.dto.response.LoginSuccessResponse;
 import edutrack.user.dto.response.UserDataResponse;
 import edutrack.user.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -29,14 +33,13 @@ public class AuthController {
     private final JwtUtils jwtUtils;
     private final AuthService authService;
     private final AuthenticationManager authenticationManager;
+    private final RefreshTokenService refreshTokenService;
 
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
-    @PostMapping("/auth/signup")
+    @PostMapping("/signup")
     @Operation(summary = "Register a new user", description = "Registers a new user using an invite code and user details.")
-    public ResponseEntity<UserDataResponse> registerUser(
-            @RequestParam String invite,
-            @RequestBody @Valid UserRegisterRequest userRequest) {
+    public ResponseEntity<UserDataResponse> registerUser(@RequestParam String invite, @RequestBody @Valid UserRegisterRequest userRequest) {
         try {
             UserDataResponse response = authService.registerUser(invite, userRequest);
             return ResponseEntity.ok(response);
@@ -45,7 +48,7 @@ public class AuthController {
         }
     }
 
-    @PostMapping("/auth/signin")
+    @PostMapping("/signin")
     @Operation(summary = "User login", description = "Logs in the user and returns a token for authentication.")
     public ResponseEntity<?> loginUser(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response){
         logger.info("User attempting to log in with email: {}", loginRequest.getEmail());
@@ -56,14 +59,18 @@ public class AuthController {
 
         // get info about user
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        logger.info("User '{}' logged in successfully", userDetails.getUsername()); // username = email
 
-        logger.info("User '{}' logged in successfully", userDetails.getUsername());
-
-        // generate JWT cookie after success authorization
+        // generate JWT access token after and add to cookie
         ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails); // generate cookie (with JWT)
         response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());   // HttpCookie packing for storage
 
-        logger.info("JWT token added to response for user '{}'", userDetails.getUsername());
+        // create refresh token and add to cookie
+        RefreshTokenEntity refreshToken = refreshTokenService.createRefreshToken(userDetails.getUsername());    // username = email
+        ResponseCookie jwtRefreshCookie = jwtUtils.generateRefreshJwtCookie(refreshToken.getToken());
+        response.addHeader(HttpHeaders.SET_COOKIE, jwtRefreshCookie.toString());
+
+        logger.info("JWT and Refresh tokens added to response for user '{}'", userDetails.getUsername());
 
         // Entity mapping to DTO and added token to response
         LoginSuccessResponse loginResponse = authService.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword());
@@ -75,8 +82,39 @@ public class AuthController {
     @PostMapping("/signout")
     @Operation(summary = "User logout", description = "Logs out the user by clearing the JWT cookie.")
     public ResponseEntity<?> logoutUser() {
-        ResponseCookie cookie = jwtUtils.getCleanJwtCookie(); // return the same cookie, but without JWT
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+        Object principle = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (!"anonymousUser".equals(principle.toString())) {
+            String userEmail = ((UserDetailsImpl) principle).getUsername(); // get email
+            refreshTokenService.deleteByUserEmail(userEmail);               // delete refresh by email
+        }
+
+        // Clean JWT and Refresh tokens
+        ResponseCookie jwtCookie = jwtUtils.getCleanJwtCookie();
+        ResponseCookie jwtRefreshCookie = jwtUtils.getCleanJwtRefreshCookie();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())        // return the same cookie, but without JWT
+                .header(HttpHeaders.SET_COOKIE, jwtRefreshCookie.toString()) // return the same refresh cookie, but without JWT
                 .body("You've been signed out!");
+    }
+
+    @PostMapping("/refreshtoken")
+    @Operation(summary = "Refresh JWT Token", description = "Generates new JWT access and refresh tokens")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
+        String refreshToken = jwtUtils.getJwtRefreshFromCookies(request);;
+;
+        if ((refreshToken != null) && (refreshToken.length() > 0)) {
+            return refreshTokenService.findByToken(refreshToken)
+                    .map(refreshTokenService::verifyExpiration)
+                    .map(tokenEntity -> {
+                        String userEmail = tokenEntity.getUserEmail();
+                        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userEmail);
+                        return ResponseEntity.ok()
+                                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                                .body("Token is refreshed successfully!");
+                    })
+                    .orElseThrow(() -> new RuntimeException("Refresh token is not in database!"));
+        }
+        return ResponseEntity.badRequest().body("Refresh Token is empty!");
     }
 }
