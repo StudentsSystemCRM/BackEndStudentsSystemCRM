d822c7c87da73bc89f1ab6e0d68a9f737436eb0d
Dmitrii <zhirov.i.am@gmail.com>
2024-09-18 23:42:08 +0300

    ver.1.4.1 - upd CORS, application.properties, fix problems with jwt expired error

--- src/main/java/edutrack/configuration/CORSConfiguration.java
+++ src/main/java/edutrack/configuration/CORSConfiguration.java
@@ -1,16 +1,28 @@
 package edutrack.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
-public class CORSConfiguration implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+public class CORSConfiguration {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        config.setExposedHeaders(List.of(HttpHeaders.SET_COOKIE));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
