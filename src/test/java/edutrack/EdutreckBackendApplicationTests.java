b56e01b9b471d656f78bde01a792ad87c777709f
Ekaterina Goncharenko <eg93006@gmail.com>
2024-09-19 16:27:02 +0300

    fix tests

--- src/test/java/edutrack/EdutreckBackendApplicationTests.java
+++ src/test/java/edutrack/EdutreckBackendApplicationTests.java
@@ -1,15 +1,12 @@
 package edutrack;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
-class EdutreckBackendApplicationTests {
+public class EdutreckBackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
-
 }
