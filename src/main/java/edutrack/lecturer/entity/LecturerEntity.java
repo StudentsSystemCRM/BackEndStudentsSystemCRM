d2b65d774eddb9020788dc3a183bb7e8347262c2
Larisa Becket <lbikteeva@gmail.com>
2024-09-23 14:30:33 +0300

    Finished writing tests for Lecturer service

--- src/main/java/edutrack/lecturer/entity/LecturerEntity.java
+++ src/main/java/edutrack/lecturer/entity/LecturerEntity.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Set;
 
@@ -17,17 +18,30 @@ import java.util.Set;
 @AllArgsConstructor
 @Entity
 @Table(name = "lecturers")
+@EntityListeners(AuditingEntityListener.class)  // Включаем аудитирование
 public class LecturerEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String city;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)  // status обязателен
     private LecturerStatus status;
 
     @ManyToMany
@@ -36,12 +50,13 @@ public class LecturerEntity {
             joinColumns = @JoinColumn(name = "lecturer_id"),
             inverseJoinColumns = @JoinColumn(name = "group_id"))
     private Set<GroupEntity> groups;
-    
-    @CreatedBy
-    @Column(name = "created_by") 
-    String createdBy; 
-    @LastModifiedBy 
-    @Column(name = "updated_by")
-    String lastModifiedBy;
 
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String lastModifiedBy;
 }
