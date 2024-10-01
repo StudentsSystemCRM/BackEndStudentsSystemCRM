55e2a193265dc2a11485c9ab593de0a575c0b0f2
Dmitrii <zhirov.i.am@gmail.com>
2024-10-02 00:06:52 +0300

    error work complete

--- src/main/java/edutrack/security/jwt/JwtRequestFilter.java
+++ src/main/java/edutrack/security/jwt/JwtRequestFilter.java
@@ -33,25 +33,39 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
-
         String email = null;
         String jwt = null;
 
+        // to handle the case if the access token has expired and we cannot execute 'signOut'
+        boolean isSignOutRequest = request.getRequestURI().equalsIgnoreCase("/api/auth/signout");
+
+        // Checking token availability
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.replace("Bearer ", "");
 
             try {
-                // validation token and extract email
+                // Token validation attempt
                 Claims claims = jwtTokenProvider.validateAccessToken(jwt);
                 email = claims.getSubject();
+            } catch (ExpiredJwtException e) {
+                // If the token has expired but the request is for 'signout', continue processing
+                // I know it's not the best solution, but I don't have any more ideas - Zhirov Dmitrii
+                if (isSignOutRequest) {
+                    //Retrieving an email from an expired token
+                    email = e.getClaims().getSubject();
+                } else {
+                    throw new AccessException("Access token expired");
+                }
+            }
 
-                // check if the user exists in the database
+            // If the email is received, continue processing the request
+            if (email != null) {
                 UserEntity user = accountRepository.findByEmail(email);
                 if (user == null) {
                     throw new AccessException("User not found in the database.");
                 }
 
-                // set authentication with roles
+                // Set up authentication
                 if (SecurityContextHolder.getContext().getAuthentication() == null) {
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                             email, null, AuthorityUtils.createAuthorityList(user.getRoles().stream().map(r -> "ROLE_" + r).toArray(String[]::new))
@@ -59,19 +73,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                     authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
-            } catch (ExpiredJwtException e) {
-                throw new AccessException("Access token expired");
-            } catch (Exception e) {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                response.getWriter().write("Invalid access token");
-                response.getWriter().flush();
-                return;
             }
         } else {
             throw new IllegalArgumentException("Authorization header is missing or invalid");
         }
 
+        // Skip filter execution for all requests that require authentication
         filterChain.doFilter(request, response);
     }
 
