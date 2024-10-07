e4a0cc1841abdd8ddf40715fd1f31d1ccfb1d756
Ekaterina Goncharenko <eg93006@gmail.com>
2024-10-07 22:10:22 +0300

    add log fail singin

--- src/main/java/edutrack/elasticsearch/service/ElasticsearchLogging.java
+++ src/main/java/edutrack/elasticsearch/service/ElasticsearchLogging.java
@@ -2,12 +2,12 @@ package edutrack.elasticsearch.service;
 
 public interface ElasticsearchLogging {
 
-	public String logError(
-			String errorId,
+	public String saveLog(
 			String message, 
 			String stackTraceAsString,
 			String requestUrl, 
 			String requestMethod, 
 			String username);
+	public String saveLogExeption(Exception ex);
 	void deleteOldLogs();
 }
