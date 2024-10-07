035be98b1433ffb2b070dd28f7929b51d2098f3c
Ekaterina Goncharenko <eg93006@gmail.com>
2024-10-07 20:20:55 +0300

    add fields to Elasticsearch

--- src/main/java/edutrack/elasticsearch/service/ElasticsearchLogging.java
+++ src/main/java/edutrack/elasticsearch/service/ElasticsearchLogging.java
@@ -2,5 +2,12 @@ package edutrack.elasticsearch.service;
 
 public interface ElasticsearchLogging {
 
-	public String logError(String errorId, String message, String stackTrace);
+	public String logError(
+			String errorId,
+			String message, 
+			String stackTraceAsString,
+			String requestUrl, 
+			String requestMethod, 
+			String username);
+	void deleteOldLogs();
 }
