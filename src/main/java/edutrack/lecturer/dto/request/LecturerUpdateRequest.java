67773d0b875bfda6cc48d673c2288bcea03a0e18
Larisa Becket <lbikteeva@gmail.com>
2024-10-14 12:48:11 +0300

    Refactor LecturerUpdateRequest: Make fields optional and fix test errors

--- src/main/java/edutrack/lecturer/dto/request/LecturerUpdateRequest.java
+++ src/main/java/edutrack/lecturer/dto/request/LecturerUpdateRequest.java
@@ -1,15 +1,16 @@
 package edutrack.lecturer.dto.request;
 
 import edutrack.lecturer.constant.LecturerStatus;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Set;
+
 import static edutrack.user.constant.ValidAccountConstant.*;
 import static edutrack.user.constant.ValidationAccountingMessage.*;
+
 
 @Data
 @NoArgsConstructor
@@ -18,30 +19,22 @@ public class LecturerUpdateRequest {
     @NotNull
     private Long id;
 
-    @NotBlank(message = "First name is mandatory")
     @Pattern(regexp = NAME_PATTERN, message = INVALID_NAME)
     private String firstName;
 
-    @NotBlank(message = "Last name is mandatory")
     @Pattern(regexp = NAME_PATTERN, message = INVALID_NAME)
     private String lastName;
 
-    @NotBlank(message = "Phone number is mandatory")
     @Pattern(regexp = PHONE_NUMBER_PATTERN, message = INVALID_PHONE)
     private String phoneNumber;
 
-    @NotBlank(message = "Email is mandatory")
     @Pattern(regexp = EMAIL_PATTERN, message = INVALID_EMAIL)
     private String email;
 
-    @NotBlank(message = "City is mandatory")
-    @Pattern(regexp = CITY_PATTERN, message = INVALID_CITY)
+    @Pattern(regexp = CITY_PATTERN, message = "Invalid city")
     private String city;
 
-    @NotNull(message = "Status is mandatory")
     private LecturerStatus status;
 
     private Set<Long> groupIds;
 }
-
-
