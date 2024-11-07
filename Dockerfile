6daf051b62230bae2b77236dcaa4bd9e198f9f04
Ekaterina Goncharenko <eg93006@gmail.com>
2024-11-08 00:38:32 +0200

    add opentelenetry

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,24 @@ WORKDIR /app
 COPY . .
 RUN mvn clean package -DskipTests
 
-
+# Runtime stage
 FROM eclipse-temurin:21-jre-alpine
 WORKDIR /app1
 COPY --from=build /app/target/edutreck_backend-0.0.1-SNAPSHOT.jar edutreck_backend.jar
+COPY ./opentelemetry-javaagent.jar .
+COPY ./CustomTracesExporter-0.0.1.jar .
 
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "edutreck_backend.jar"]
+
+# Указываем параметры JVM при запуске
+ENTRYPOINT ["java", \
+            "-javaagent:./opentelemetry-javaagent.jar", \
+            "-Dotel.javaagent.extensions=./CustomTracesExporter-0.0.1.jar", \
+            "-Dotel.traces.exporter=none", \
+            "-Dotel.metrics.exporter=none", \
+            "-Dotel.logs.exporter=none", \
+            "-Djava.security.egd=file:/dev/./urandom", \
+            "-Dspans.destination.url=http://host.docker.internal:24224", \
+            "-Dlogs.destination.url=http://host.docker.internal:24225", \
+            "-Dmetrics.destination.url=http://host.docker.internal:24226", \
+            "-jar", "edutreck_backend.jar"]
--- src/main/java/edutrack/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/edutrack/exception/handler/GlobalExceptionHandler.java
@@ -3,21 +3,6 @@ package edutrack.exception.handler;
 import java.util.List;
 import java.util.UUID;
 
-import edutrack.emailService.exception.EmailServiceException;
-import edutrack.emailService.exception.MailgunBadRequestException;
-import edutrack.emailService.exception.TriggerNotFoundException;
-import edutrack.exception.*;
-import edutrack.exception.response.GeneralErrorResponse;
-import edutrack.exception.response.GeneralErrorResponseValidationDto;
-import edutrack.group.exception.GroupNotFoundException;
-import edutrack.student.exception.EmailAlreadyInUseException;
-import edutrack.user.exception.AccessException;
-import edutrack.user.exception.AccessRoleException;
-import edutrack.user.exception.InvalidDateFormatException;
-import edutrack.user.exception.ResourceExistsException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -28,111 +13,140 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+import edutrack.emailService.exception.EmailServiceException;
+import edutrack.emailService.exception.MailgunBadRequestException;
+import edutrack.emailService.exception.TriggerNotFoundException;
+import edutrack.exception.StudentNotFoundException;
+import edutrack.exception.response.GeneralErrorResponse;
+import edutrack.exception.response.GeneralErrorResponseValidationDto;
+import edutrack.group.exception.GroupNotFoundException;
+import edutrack.student.exception.EmailAlreadyInUseException;
+import edutrack.user.exception.AccessException;
+import edutrack.user.exception.AccessRoleException;
+import edutrack.user.exception.InvalidDateFormatException;
+import edutrack.user.exception.ResourceExistsException;
+import lombok.extern.slf4j.Slf4j;
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public GeneralErrorResponseValidationDto handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
 		List<String> messages = ex.getAllErrors()
 				.stream()
 				.map(DefaultMessageSourceResolvable::getDefaultMessage)
 				.toList();
-		return new GeneralErrorResponseValidationDto(UUID.randomUUID().toString(), messages);
+		return new GeneralErrorResponseValidationDto(idResponseError, messages);
 	}
 
 	@ExceptionHandler(HandlerMethodValidationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public GeneralErrorResponseValidationDto handleConstraintViolationException(HandlerMethodValidationException ex) {
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
 		List<String> messages = ex.getAllErrors()
 				.stream()
 				.map(MessageSourceResolvable::getDefaultMessage)
 				.toList();
-		return new GeneralErrorResponseValidationDto(UUID.randomUUID().toString(), messages);
+		return new GeneralErrorResponseValidationDto(idResponseError, messages);
 	}
 
 	@ExceptionHandler(InvalidDateFormatException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public GeneralErrorResponse handleInvalidDateFormatException(InvalidDateFormatException ex) {
-		logger.error("Invalid Date Format Exception occurred.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error("Invalid Date Format Exception occurred."  + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(ResourceExistsException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public GeneralErrorResponse handleResourceExistsException(ResourceExistsException ex) {
-		logger.error("Resource already exists.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(AccessRoleException.class)
 	@ResponseStatus(HttpStatus.FORBIDDEN)
 	public GeneralErrorResponse handleAccessException(AccessRoleException ex) {
-		logger.error("Access exception.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 	
 	@ExceptionHandler(AccessException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	public GeneralErrorResponse handleAccessException(AccessException ex) {
-		logger.error("Access exception.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(StudentNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public GeneralErrorResponse handleStudentNotFoundException(StudentNotFoundException ex) {
-		logger.error("Student not found.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(EmailAlreadyInUseException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public GeneralErrorResponse handleEmailAlreadyInUseException(EmailAlreadyInUseException ex) {
-		logger.error("Email already in use.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(TriggerNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public GeneralErrorResponse handleTriggerNotFoundException(TriggerNotFoundException ex) {
-		logger.error("TriggerNotFoundException", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(MailgunBadRequestException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public void handleMailgunBadRequestException(MailgunBadRequestException ex) {
-		logger.error("MailgunBadRequestException", ex);
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
 	}
 
 	@ExceptionHandler(EmailServiceException.class)
 	public GeneralErrorResponse handleEmailServiceException(EmailServiceException ex) {
-		logger.error("EmailServiceException", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(GroupNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public GeneralErrorResponse handleJwtTokenExpiredException(GroupNotFoundException ex) {
-		logger.error("GroupNotFoundException", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(NoResourceFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public GeneralErrorResponse handleJwtTokenExpiredException(NoResourceFoundException ex) {
-		logger.error("Endpoints doesn't exists", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, ex.getMessage());
 	}
 
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public GeneralErrorResponse handleException(Exception ex) {
-		logger.error("An unexpected error occurred.", ex);
-		return new GeneralErrorResponse(UUID.randomUUID().toString(), "An unexpected error: " + ex.getMessage());
+		String idResponseError = UUID.randomUUID().toString();
+		log.error(ex.getMessage() + " , id response error: " + idResponseError, ex);
+		return new GeneralErrorResponse(idResponseError, "An unexpected error: " + ex.getMessage());
 	}
 
 }
